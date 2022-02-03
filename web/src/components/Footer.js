1ba634816150131fe7628be12429e667291759b0
HenriqueBS0 <henrique.10agr@gmail.com>
2022-02-03 08:18:04 -0300

    Criado Footer

---
+++ web/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="p-5 flex justify-center">
+            <span className="text-slate-400">&#169; copyright 2022</span>
+        </footer>
+    );
+}
